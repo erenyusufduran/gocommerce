8fa17d04a9cf8545b0439e92ba924224872db4da
Eren Duran <eren.duran@colins.com>
2024-04-28 15:46:18 +0300

    fix errors hash functions and searchproduct

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -7,18 +7,40 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/erenyusufduran/gocommerce/database"
+
 	"github.com/erenyusufduran/gocommerce/models"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func HashPassword(password string) string {
+var UserCollection *mongo.Collection = database.UserData(database.Client, "Users")
+var ProductCollection *mongo.Collection = database.ProductData(database.Client, "Products")
+var Validate = validator.New()
 
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
 }
 
 func VerifyPassword(userPassword, givenPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(givenPassword), []byte(userPassword))
+	valid := true
+	msg := ""
 
+	if err != nil {
+		msg = "Login or Password is incorrect"
+		valid = false
+	}
+
+	return valid, msg
 }
 
 func Signup() gin.HandlerFunc {
@@ -64,21 +86,21 @@ func Signup() gin.HandlerFunc {
 
 		password := HashPassword(*user.Password)
 		user.Password = &password
-		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.ID = primitive.NewObjectID()
 		user.User_ID = user.ID.Hex()
 
 		token, refreshToken := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, user.User_ID)
 		user.Token = &token
-		user.Refrest_Token = &refreshToken
+		user.Refresh_Token = &refreshToken
 		user.UserCart = make([]models.ProductUser, 0)
 		user.Address_Details = make([]models.Address, 0)
 		user.Order_Status = make([]models.Order, 0)
 
 		_, insertErr := UserCollection.InsertOne(ctx, user)
 		if insertErr != nil {
-			c.JSON(http.StatusInternalServerError, gin.H["error":"the user could not created"])
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "the user could not created"})
 			return
 		}
 
@@ -99,13 +121,13 @@ func Login() gin.HandlerFunc {
 
 		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H["error":"login or password is incorrect"])
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password is incorrect"})
 			return
 		}
 
 		isValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
 		if !isValid {
-			c.JSON(http.StatusInternalServerError, gin.H["error", msg])
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			fmt.Println(msg)
 			return
 		}
@@ -117,13 +139,42 @@ func Login() gin.HandlerFunc {
 }
 
 func ProductViewerAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {}
 
 }
 
 func SearchProduct() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var productList []models.Product
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		cursor, err := ProductCollection.Find(ctx, bson.D{{}})
+		defer cursor.Close(ctx)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "something went wrong, please try again")
+			return
+		}
+
+		err = cursor.All(ctx, &productList)
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if err := cursor.Err(); err != nil {
+			log.Println(err)
+			c.IndentedJSON(400, "invalid")
+			return
+		}
+
+		c.IndentedJSON(200, productList)
+
+	}
 }
 
 func SearchProductByQuery() gin.HandlerFunc {
+	return func(c *gin.Context) {}
 
 }
