d18c4af54f2ebb6b9dc7aa6e60c47edf2c71558f
Eren Duran <eren.duran@colins.com>
2024-04-28 05:48:42 +0300

    db setup

---
+++ database/databasetup.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://foo:bar@localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb")
+		return nil
+	}
+
+	fmt.Println("Successfully connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	return client.Database("gocommerce").Collection(collectionName)
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	return client.Database("gocommerce").Collection(collectionName)
+}
